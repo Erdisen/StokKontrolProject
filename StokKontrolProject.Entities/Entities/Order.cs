56be224e5de56efbe9c0a833234c07c292785b44
Erdisen <erdisen16@gmail.com>
2023-01-14 11:58:41 +0300

    Add project files.

---
+++ StokKontrolProject.Entities/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokKontrolProject.Entities.Entities
+{
+    public class Order :BaseEntity
+    {
+        public Order()
+        {
+            SiparisDetaylari = new List<OrderDetails>();
+        }
+        [ForeignKey("Kullanici")]
+        public int UserID { get; set; }
+
+
+        //Navigation Properties
+        public virtual User Kullanici { get; set; }
+        public virtual List<OrderDetails> SiparisDetaylari { get; set; }
+    }
+}
---
+++ StokKontrolProject.Repositories/Context/StokKontrolContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StokKontrolProject.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokKontrolProject.Repositories.Context
+{
+    public class StokKontrolContext : DbContext
+    {
+        public StokKontrolContext(DbContextOptions<StokKontrolContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JH0OB08; Database=StokKontrolDB; uid = sa; pwd=1;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
