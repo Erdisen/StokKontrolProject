5e66f5c6a27fe9c2a21c64a08499521f6a66d55c
Erdisen <erdisen16@gmail.com>
2023-01-15 14:48:48 +0300

    WEB.UI katmanında Admin Area oluşturuldu.

--- StokKontrolProject.UI/Program.cs
+++ StokKontrolProject.UI/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
@@ -19,6 +19,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
 
 app.MapControllerRoute(
     name: "default",
